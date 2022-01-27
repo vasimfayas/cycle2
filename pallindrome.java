cece08057e3dfe61cfb34a4cf4be294958f392c0
vasimfayas <77981149+vasimfayas@users.noreply.github.com>
2022-01-27 21:46:46 +0530

    Add files via upload

---
+++ pallindrome.java
@@ -0,0 +1,23 @@
+
+//checking wheather a string is pallindrome or not
+import java.util.*;
+
+class pallindrome {
+    public static void main(String args[]) {
+        try (Scanner obj = new Scanner(System.in)) {
+            System.out.println("Enter the string:");
+            String name = obj.nextLine();
+            String rev = "";
+            int len = name.length();
+            for (int i = len - 1; i >= 0; i--) {
+                rev = rev + name.charAt(i);
+            }
+            System.out.println("Reversed string is:" + rev);
+            if (name.equals(rev)) {
+                System.out.println("It is pallindrome");
+            } else {
+                System.out.println("It is not pallindrome");
+            }
+        }
+    }
+}
